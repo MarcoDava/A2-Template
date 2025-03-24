cc1feb6859d7244f403348d1a7e10bcd78641ce0
MarcoDava <marco.a.dava@gmail.com>
2025-03-23 20:20:54 -0400

    removing unecessary classes

--- src/main/java/ca/mcmaster/se2aa4/island/team012/Runner.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team012/Runner.java
@@ -5,7 +5,6 @@ import static eu.ace_design.island.runner.Runner.run;
 import java.io.File;
 
 import ca.mcmaster.se2aa4.island.team012.DroneComponents.Drone;
-import ca.mcmaster.se2aa4.island.team012.DroneComponents.TemplateExplorer;
 
 public class Runner {
 
